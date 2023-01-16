6c7acca5aedb16335fdf1abb7ade2c6cbd001cd4
Joe Kaiser <kaiserfjoe@gmail.com>
2023-01-16 14:49:05 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    if (string == '') {
+        return '';
+    }
+    const reversed = [];
+    const splitString = string.split("");
+    for (let i = string.length-1; i >= 0; i--) {
+        reversed.push(string[i]);
+    }
 
+    return reversed.join("");
 };
 
 // Do not edit below this line
