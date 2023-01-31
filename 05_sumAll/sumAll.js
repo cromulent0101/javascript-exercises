e943753b93b69b44c2c52b8e0354855e8ca518ba
Joe Kaiser <kaiserfjoe@gmail.com>
2023-01-30 22:30:29 -0500

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start > end) { // swap variables if not passed properly
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    if ((start < 0) | (end < 0)) {
+        return 'ERROR';
+    }
+    if ((typeof(start) != 'number') | (typeof(end) != 'number')) {
+        return 'ERROR';
+    }
+    let sum = 0
+    while (start <= end) {
+        sum = sum + start;
+        start++;
+    }
+    return sum;
+
 
 };
 
